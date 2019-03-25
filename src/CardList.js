085809489b0c0760ca849475fdd0a3b9c5c7d5c0
Johan Uribe <Johanandresuribe@gmail.com>
2019-03-25 01:00:34 -0400

    added a remove button to cards

--- src/CardList.js
+++ src/CardList.js
@@ -4,16 +4,18 @@ import Kanji from './Kanji.js';
 // Need to use the fetch call in app.js to update wordList with the characters. 
 // wordList and kanjiList are both arrays.
 // Make them an array filled with objects eventually.
-const CardList = ({kanjiList}) => {
+const CardList = ({kanjiList, onClickDelete}) => {
   return (
     <div>
       {
         kanjiList.map( (word, i) => {
           return (
             <Kanji // passes these vales to Kanji component to build
-              key = {i}
+              id = {i}
+              key = {i} // can not pass key as a prop because it is a react specific keyword
               word = {kanjiList[i].word}
               kanji = {kanjiList[i].kanji}
+              onClick = {onClickDelete}
             />
           );
         })
--- src/Kanji.js
+++ src/Kanji.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 // takes the props 'key', 'word', and 'kanji', where key is just the i for each card.
-const Kanji = ({word, kanji}) => {
+const Kanji = ({id, word, kanji, onClick}) => {
   return (
-    <div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <div>
+    <div className = 'tc bg-light-green dib br3 pa3 ma2 shadow-hover bw2 shadow-5'>
         <h2 className = 'f1'>{kanji}</h2>
         <p> {word}</p>
-      </div>
+        <button 
+          type = "button"
+          id = {word}
+          onClick = {onClick}
+          className = 'f7 bg-light-blue br3' 
+          >
+          remove
+        </button>
     </div>
   );
 };
