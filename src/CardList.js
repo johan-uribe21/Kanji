7f4b8622c431a308dd58add7f2cf8690f31f17e4
Johan Uribe <Johanandresuribe@gmail.com>
2019-04-09 00:02:06 -0400

    finished refactoring the code, fixed the error related to promises in the populateInitialList, and simplified all the methods calls, and refactored the submit form into a React controlled componenet

--- src/CardList.js
+++ src/CardList.js
@@ -1,35 +1,24 @@
 import React from 'react';
 import Kanji from './Kanji.js';
 
-// Need to use the fetch call in app.js to update wordList with the characters. 
-// wordList and kanjiList are both arrays.
-// Make them an array filled with objects eventually.
 const CardList = ({wordList, onClickDelete, isLoaded}) => {
   const list = wordList || []
 
   return (
     <div>
       {
-        list.length > 0 ? wordList.map((char, idx) => {
-            return <Kanji key={idx} word={char.word} kanji={char.kanji} onClickDelete={onClickDelete}/>
-        }) : (<span>I got nothing</span>)
+        list.length > 0 ? 
+        wordList.map((char, idx) => {
+            return <Kanji 
+              key={idx} 
+              word={char.word} 
+              kanji={char.kanji} 
+              onClickDelete={onClickDelete}/>
+        }) : 
+        (<span>I got nothing</span>)
       }
     </div>
   );
 };
 
 export default CardList
-
-// {
-      //   wordList.map( (word, i) => {
-      //     return (
-      //       <Kanji // passes these vales to Kanji component to build
-      //         id = {i}
-      //         key = {i} // can not pass key as a prop because it is a react specific keyword
-      //         word = {word[i].word}
-      //         kanji = {word[i].kanji}
-      //         onClick = {onClickDelete}
-      //       />
-      //     );
-      //   })
-      // }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -14,21 +14,12 @@ class SearchBox extends React.Component  {
   // updates the input field as the user types into input
   handleChange = (event) => {
     this.setState({input: event.target.value});
-    // if (event.key === "Enter") {
-    //   let input = document.getElementById('inputKanji').value
-    //   let newWordList = this.state.wordList;
-    //   newWordList.push(input);
-    //   this.setState( {wordList: newWordList} );
-    //   document.getElementById('inputKanji').value = '';
-    //   // calls fetchKanji every time new kanji is submitted to update set and trigger a rerender with new card.
-    //   this.fetchKanji();
-    //   }
   };
 
   handleSubmit(event) {
     event.preventDefault();
-    // pass the state.input
-    this.props.callAPI(this.state.input)
+    this.props.submitWord(this.state.input);
+    this.setState({input: ''});
   };
 
   render() {
@@ -36,12 +27,13 @@ class SearchBox extends React.Component  {
       <form 
         className = 'pa3 ba b--green bg-lightest-blue'
         onSubmit = {this.handleSubmit} >
-        <label>Addition Vocabulary Words
+        <label>Additional Vocabulary Words: 
           <input 
             id="inputKanji" 
-            ype="text" 
+            type="text" 
             size="20"
             onChange={this.handleChange}
+            value = {this.state.input} // for controlled component, state controls values
             />
         </label>
         <input 
