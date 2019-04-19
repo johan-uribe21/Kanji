a54b12561f125310602af2166e29e62088925b49
Johan Uribe <Johanandresuribe@gmail.com>
2019-04-19 00:53:21 -0400

    Updates

---
+++ precache-manifest.e9043c9a4c9747a284dd69dd314eddcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e6f87c1082b9b84849d",
+    "url": "/Kanji/static/css/main.346651f8.chunk.css"
+  },
+  {
+    "revision": "0e6f87c1082b9b84849d",
+    "url": "/Kanji/static/js/main.c1753d3a.chunk.js"
+  },
+  {
+    "revision": "a7450414552d0e2df3bf",
+    "url": "/Kanji/static/js/runtime~main.aa11725c.js"
+  },
+  {
+    "revision": "9c639412a18ead342b6a",
+    "url": "/Kanji/static/css/2.61f5cbbb.chunk.css"
+  },
+  {
+    "revision": "9c639412a18ead342b6a",
+    "url": "/Kanji/static/js/2.33c62ecc.chunk.js"
+  },
+  {
+    "revision": "a18a746947623cb23a4402ae5579d263",
+    "url": "/Kanji/index.html"
+  }
+];
